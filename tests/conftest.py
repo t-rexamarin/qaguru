03e5f85a11fc3ac1dbb2adfb78314fc015cfa111
Roman Khoroshev <r.khoroshev@atol.ru>
2023-07-06 22:09:04 +0300

    [lesson 9] - настройки хрома

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(autouse=True)
 def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    browser.config.driver_options = options
     browser.config.driver.maximize_window()
