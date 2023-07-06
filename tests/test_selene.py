d04a601436a5408286b93e56ac6fa2b83825ebc2
Roman Khoroshev <r.khoroshev@atol.ru>
2023-07-06 23:05:22 +0300

    [lesson 9] - test fix

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,12 +1,13 @@
 import allure
+from selene import browser
 from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared.jquery_style import s
 
 
 @allure.story("Находим в репозитории issue с номером 76")
-def test_github(driver):
-    driver.open("https://github.com")
+def test_github():
+    browser.open("https://github.com")
 
     s(".header-search-input").click()
     s(".header-search-input").send_keys("eroshenkoam/allure-example")
