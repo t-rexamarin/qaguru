2ad2d36e1e5936a2b8043e1db3e78a950bf88384
Roman Khoroshev <r.khoroshev@atol.ru>
2023-07-09 12:46:27 +0300

    [lesson 9] - driver settings fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
     browser.config.driver_options = options
     browser.config.driver.maximize_window()
     yield browser
