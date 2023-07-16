c54a66d87799861791055dd85ab3bf4c92a87662
Roman Khoroshev <r.khoroshev@atol.ru>
2023-07-16 13:57:50 +0300

    [lesson 12] - init

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,48 @@
+import os
 import datetime
 
 import pytest
-from selene import browser, Browser
+from dotenv import load_dotenv
+from selene import Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 from qaguru.user import User, Hobbies
+from qaguru.utils import attach, constants
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup() -> Browser:
+    browser_version = os.getenv(constants.BROWSER_VERSION)
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv(constants.LOGIN)
+    password = os.getenv(constants.PASSWORD)
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser = Browser(Config(driver))
     browser.driver.maximize_window()
     yield browser
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()
 
 
