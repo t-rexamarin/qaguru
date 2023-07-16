9f59e308efc5ff42d82fb81499cb1c4f50548ac1
Roman Khoroshev <r.khoroshev@atol.ru>
2023-07-16 15:40:11 +0300

    [lesson 12] - temporary test

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,8 +1,43 @@
+import allure
+from selene import have, by
+
 from qaguru.practice_form import PracticeForm
 
 
 def test_registration_form(user_data, browser_setup):
-    PracticeForm(browser=browser_setup)\
-        .open()\
-        .register(user=user_data)\
-        .should_have_registered(user=user_data)
+    # PracticeForm(browser=browser_setup)\
+    #     .open()\
+    #     .register(user=user_data)\
+    #     .should_have_registered(user=user_data)
+    first_name = "Alex"
+    last_name = "Egorov"
+    browser = browser_setup
+    with allure.step("Open registrations form"):
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+        browser.driver.execute_script("$('footer').remove()")
+        browser.driver.execute_script("$('#fixedban').remove()")
+
+    with allure.step("Fill form"):
+        browser.element("#firstName").set_value(first_name)
+        browser.element("#lastName").set_value(last_name)
+        browser.element("#userEmail").set_value("alex@egorov.com")
+        browser.element("#genterWrapper").element(by.text("Other")).click()
+        browser.element("#userNumber").set_value("1231231230")
+        # browser.element("#dateOfBirthInput").click()
+        # browser.element(".react-datepicker__month-select").s("July")
+        # browser.element(".react-datepicker__year-select").selectOption("2008")
+        # browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
+        browser.element("#subjectsInput").send_keys("Maths")
+        browser.element("#subjectsInput").press_enter()
+        browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
+        # browser.element("#uploadPicture").uploadFromClasspath("img/1.png")
+        browser.element("#currentAddress").set_value("Some street 1")
+        browser.element("#state").click()
+        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+        browser.element("#city").click()
+        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+        browser.element("#submit").click()
+
+    with allure.step("Check form results"):
+        browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
