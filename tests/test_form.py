b71c8df4cadab17998c8449ceb8f53d34103f621
Roman Khoroshev <r.khoroshev@atol.ru>
2023-07-17 10:21:43 +0300

    [lesson 12] - add steps

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,8 +1,15 @@
+import allure
+
 from qaguru.practice_form import PracticeForm
 
 
 def test_registration_form(user_data, browser_setup):
-    PracticeForm(browser=browser_setup)\
-        .open()\
-        .register(user=user_data)\
-        .should_have_registered(user=user_data)
+    practice_form = PracticeForm(browser=browser_setup)
+    with allure.step("Открываем форму"):
+        practice_form.open()
+
+    with allure.step("Заполняем форму данными и отправляем"):
+        practice_form.register(user=user_data)
+
+    with allure.step("Проверить что форма успешно отправлена"):
+        practice_form.should_have_registered(user=user_data)
